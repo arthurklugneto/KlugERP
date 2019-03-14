0d1326912176a6e2e6b884ed736ac8da710868ba
Arthur Klug Neto <arthur.klug@fh.company>
2019-03-14 11:33:15 -0300

    TCCERP-005 : Create PedidoService
    CHNG <PedidoController> to use the newly created service
    CHNG <PedidoService> to encapsulate the database crud op
         erations and give support to controller
    CHNG <index.blade.php> to proper display the situation
         of the pedido
    
    Arthur Klug Neto <arthurklugneto@gmail.com>

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -27,15 +27,15 @@ class PedidoController extends Controller
 
     public function index()
     {
-        $registros = Pedido::all();
+        $registros = $this->pedidoService->getAll();
 		return View::make('pedido.index')
 		->with('pedidos', $registros);
     }
 
     public function create()
     {
-        $clientes = Cliente::pluck('nome','id');
-		$produtos = Produto::all();
+        $clientes = $this->pedidoService->getClientes();
+		$produtos = $this->pedidoService->getProdutos();
 		
 		return View::make('pedido.create')
 		->with('clientes', $clientes)
@@ -44,31 +44,15 @@ class PedidoController extends Controller
 
     public function store(Request $request)
     {
-        $registro = new Pedido;
-		$registro->cliente_id = Input::get('cliente_id');
-		$registro->observacoes = Input::get('observacoes');
-		
-		if( empty(Input::get('dataVenda')) )
-		{
-			$registro->dataVenda = null;
-		}
-		else
-		{
-			$registro->dataVenda = Input::get('dataVenda');
-		}
-		
-		$registro->save();
-
-		$id = $registro->id;
-		
+		$id = $this->pedidoService->save(Input::all());
 		return Redirect::to('pedido/'.$id.'/edit');
     }
 
     public function edit($id)
     {
-        $produtos = Produto::pluck('nome','id');
-		$pedido = Pedido::find($id);
-		$pedidoProdutos = PedidoProduto::all()->where('pedido_id', $id);
+        $produtos = $this->pedidoService->getProdutos();
+		$pedido = $this->pedidoService->findById($id);
+		$pedidoProdutos = $this->pedidoService->getPedidoProdutos($id);
 		
 		return View::make('pedido.edit')
 		->with('produtos', $produtos)
@@ -78,15 +62,7 @@ class PedidoController extends Controller
 
     public function destroy($id)
     {
-        
-		$pedidosProdutos = PedidoProduto::all()->where('pedido_id', $id);
-		foreach ($pedidosProdutos as $key => $value)
-		{
-			$value->delete();
-		}
-		
-		$registro = Pedido::find($id);
-		$registro->delete();
+        $this->pedidoService->remove($id);		
 	
 		Session::flash('message', 'Pedido apagado com sucesso!');
 		return Redirect::to('pedido');
@@ -94,95 +70,49 @@ class PedidoController extends Controller
 
     public function finalizarPedido($id)
 	{
-		
-        $pedido = Pedido::find($id);
-        $pedido->situacao = 2;
-        $pedido->save();
-
-		$registros = Pedido::all();
+        $pedido = $this->pedidoService->finalizarPedido($id);
+        $registros = $this->pedidoService->getAll();
+        
 		return View::make('pedido.index')
 		->with('pedidos', $registros);		
 	}
 
     public function addItem($id)
 	{
+		$this->pedidoService->addItem(Input::all(),$id);
 		
-		$quantidade = Input::get('quantidade');
-		$produto = Input::get('produtos');
-		
-		$produtoPedido = Produto::find($produto);
-		
-		if( empty($quantidade) )
-		{
-			$quantidade = 1;
-		}
-		
-		$pedido = Pedido::find($id);
-				
-		$registro = new PedidoProduto;
-		$registro->pedido_id = $pedido->id;
-		$registro->quantidade = $quantidade;
-		$registro->produto_id = $produto;
-		$registro->save();
-		
-		$produtos = Produto::pluck('nome','id');
-		$pedidoProdutos = PedidoProduto::all()->where('pedido_id', $id);
+		$produtos = $this->pedidoService->getProdutos();
+        $pedidoProdutos = $this->pedidoService->getPedidoProdutos($id);
+        $pedido = $this->pedidoService->findById($id);
+        $produtoPedido = $this->pedidoService->getProduto(Input::get('produtos'));
 		
 		return View::make('pedido.edit')
 		->with('produtos', $produtos)
 		->with('pedido', $pedido)
 		->with('pedidoProdutos', $pedidoProdutos)
-		->with('produtoAtual', $produtoPedido);
-		
+		->with('produtoAtual', $produtoPedido);		
 	}
 
 	public function removeItem($id)
 	{
-		$registro = PedidoProduto::find($id);
-		$registro->delete();
-		
-		$produtos = Produto::pluck('nome','id');
-		$pedidoProdutos = PedidoProduto::all()->where('pedido_id', $registro->pedido_id);
-		$pedido = Pedido::find($registro->pedido_id);
+        $produtos = $this->pedidoService->getProdutos();
+        $pedido = $this->pedidoService->findByPedidoProdutoId($id);
+
+        $this->pedidoService->removeItem($id);
+				        
+		$pedidoProdutos = $this->pedidoService->getPedidoProdutos($pedido->id);		
 		
 		return View::make('pedido.edit')
 		->with('produtos', $produtos)
 		->with('pedido', $pedido)
-		->with('pedidoProdutos', $pedidoProdutos);
+        ->with('pedidoProdutos', $pedidoProdutos);
 	}
 
     public function venderPedido($id)
     {
-        $pedido = Pedido::find($id);
-        $pedidoProdutos = PedidoProduto::all()->where('pedido_id', $id);
-
-        $registro = new Saida;
-		$registro->cliente_id = $pedido->cliente_id;
-		$registro->valor = 0;
-		$registro->valorRecebido = 0;
-		$registro->observacoes = $pedido->observacoes;
-		$registro->dataVenda = Carbon::now();
-		$registro->save();
-
-        foreach ($pedidoProdutos as $key => $value)
-		{
-
-            $produtoVenda = Produto::find($value->produto_id);
-
-            $registroProduto = new SaidaProduto;
-            $registroProduto->saida_id = $registro->id;
-            $registroProduto->valor = $produtoVenda->precoVenda;
-            $registroProduto->quantidade = $value->quantidade;
-            $registroProduto->produto_id = $produtoVenda->id;
-            $registroProduto->save();            
-            
-		}
-
-		$id = $registro->id;
-        $pedido->situacao = 3;
-        $pedido->save();
+        $idVenda = $this->pedidoService->venderPedido($id);
 		
-		return Redirect::to('venda/'.$id.'/edit');
+		return Redirect::to('venda/'.$idVenda.'/edit');
     }
 
 }
